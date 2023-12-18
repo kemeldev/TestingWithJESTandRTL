07fc159f9031d1ca26bcebec8adf72c461abfb3b
kemeldev <kemel.developer@gmail.com>
2023-12-18 11:00:09 -0600

    First part of testing done, check the readme File

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+// tenemos que crear un virtual dom para poder hacer la prueba, por defecto jest solo ejecuta testing, por lo que usamos el enviroment the RTL para renderizar, en este caso el componente APP (ver abajo)
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+// test takes 3 arguments, first is the name use to identify the test
+// second is a function that contains the expectations to test
+// third is optional, you can define the timeout value of the execution, default is 5seconds
+
 test('renders learn react link', () => {
+  // renderizamos el app
   render(<App />);
+  // usamos la propiedad getbytext que puede recibir un texto o un regex
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -1,5 +1,6 @@
-import { render , screen } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import { Greet } from "./greet"
+
 
 // test('Greet renders correctly', ()=>{
 //   render(<Greet/>)
@@ -7,14 +8,29 @@ import { Greet } from "./greet"
 //   expect(textElement).toBeInTheDocument()
 // })
 
-test('Greet renders correctly', ()=> {
-  render(<Greet />)
-  const textElement = screen.getByText('Hello')
-  expect(textElement).toBeInTheDocument()
+// descrite sirve para agrupar test, recibe 2 parámetros, el nombre y la funcion 
+describe.only('Greet', () => {
+
+  //algunas opciones  test.only para que solo testee ese
+  // test.skip, para que se salte ese. tambien se pueden usar en el bloque describe, example: describe.only, describe.skip
+
+  // la palabra test puede ser sustituida por "it" "fit" seria equivalente a test.only and "xit" equivalente a test.skip
+
+  test('Greet renders correctly', () => {
+    render(<Greet />)
+    const textElement = screen.getByText('Hello')
+    expect(textElement).toBeInTheDocument()
+  })
+
+  // un describe block puede estar anidado dentro de otro
+  describe('Nested', () => {
+    test('Greet renders with a name', () => {
+      render(<Greet name='Kevin'></Greet>)
+      const textElement = screen.getByText('Hello Kevin')
+      expect(textElement).toBeInTheDocument()
+    })
+
+  })
+
 })
 
-test('Greet renders with a name', ()=>{
-  render(<Greet name='Kevin'></Greet>)
-  const textElement = screen.getByText('Hello Kevin')
-  expect(textElement).toBeInTheDocument()
-})
